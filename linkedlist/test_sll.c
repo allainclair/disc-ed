4c12b6b2330bf18a358f1c605224769bff2e4117
Allainclair <allainclair@gmail.com>
2019-09-30 18:22:57 -0300

    auça

--- linkedlist/test_sll.c
+++ linkedlist/test_sll.c
@@ -7,7 +7,7 @@
 
 /* Test the slist_create comparing with a zero size field. */
 void test_create() {
-    Node *head;
+    Node *head = NULL;
 
     head = sllist_create();
     assert (head == NULL);
@@ -16,11 +16,13 @@ void test_create() {
 
 /* Test insertions into the list. */
 void test_insert() {
-     Node *head = NULL;
+    Node *head = NULL;
 
     printf("head %d\n", head);
-    for (int i=0; i<100; i++) {
+    for (int i=0; i<20; i++) {
         head = sllist_insert(head, i);
+        sllist_print(head);
+        printf("\n");
         // printf("%d ", head->value);
 
         // assert (list.values[i] == i);
@@ -35,13 +37,27 @@ void test_insert() {
     // assert (dlist_insert(&list, 100) == -1);
 }
 
+void test_size() {
+    Node *head = NULL;
+    int size;
+
+    for (int i=0; i<10; i++)
+        head = sllist_insert(head, i);
+    sllist_print(head);
+
+    size = sllist_size(head);
+    printf("Tamanho: %d\n", size);
+    assert (sllist_size(head) == 10);
+}
+
 
 int main() {
     test_create();
-    test_insert();
+    // test_insert();
     // test_remove();
     // test_search();
     // test_update();
     // slist_print(list);
     // printf("%d\n", ret);
+    test_size();
 }
